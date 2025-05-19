27cd7f4eef1b02a09b3265bd3be96716389b9c95
ckay19 <chbelano@my.cspc.edu.ph>
2025-05-19 15:00:45 +0800

    Add cryptographic imports and navigation menu to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,24 @@
 import streamlit as st
+import hashlib
+import base64
+import io
+import random
+from math import gcd
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Random.random import getrandbits
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding as asy_padding
 
 st.title("Applied Cryptography Application")
+
+MENU = [
+    "Symmetric Encryption/Decryption",
+    "Asymmetric Encryption/Decryption",
+    "Hashing Functions",
+    "Algorithm Informations"
+]
+choice = st.sidebar.selectbox("Navigation", MENU)
